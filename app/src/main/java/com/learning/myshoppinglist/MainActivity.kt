24d57b8c7ac0be063d26454bf672866961f5b2d9
Ravi Prakash Tiwari <109481719+RaviTiwar1@users.noreply.github.com>
2023-12-30 02:21:07 +0530

    Finish

---
+++ app/src/main/java/com/learning/myshoppinglist/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.learning.myshoppinglist
+
+import android.os.Bundle
+import android.text.Layout.Alignment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.learning.myshoppinglist.ui.theme.MyShoppingListTheme
+import com.learning.myshoppinglist.ui.theme.ShoppingListApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyShoppingListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   ShoppingListApp()
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ShoppingListPreview(){
+
+    ShoppingListApp()
+}
+
+
